f20433001976121c3d1f193c5556e14d9a30d8d8
Rahul Gupta <rahul.guptano.17@gmail.com>
2019-06-03 19:52:23 +0530

    Letto Rooms Application

---
+++ app/app/src/main/java/com/rg17/lettorooms/Features.java
@@ -0,0 +1,13 @@
+package com.rg17.lettorooms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Features extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_features);
+    }
+}
